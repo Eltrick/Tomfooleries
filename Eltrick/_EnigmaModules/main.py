2e1993474c8bbc74afec0af88993e583658a9df5
Lucifer Voeltner <lucifervoeltner03@gmail.com>
2024-05-15 18:09:56 +0700

    Enigma Modules

---
+++ Eltrick/_EnigmaModules/main.py
@@ -0,0 +1,88 @@
+ForgetEnigmaRotors = [[[["E", "K", "M", "F", "L", "G", "D", "Q", "V", "Z", "N", "T", "O", "W", "Y", "H", "X", "U", "S", "P", "A", "I", "B", "R", "C", "J"], ["A", "B", "C", "D*", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q*", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]], [["A", "J", "D", "K", "S", "I", "R", "U", "X", "B", "L", "H", "W", "T", "M", "C", "Q", "G", "Z", "N", "P", "Y", "F", "V", "O", "E"], ["A", "B", "C", "D", "E*", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R*", "S", "T", "U", "V", "W", "X", "Y", "Z"]], [["B", "D", "F", "H", "J", "L", "C", "P", "R", "T", "X", "V", "Z", "N", "Y", "E", "I", "W", "G", "A", "K", "M", "U", "S", "Q", "O"], ["A", "B", "C", "D", "E", "F", "G", "H", "I*", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V*", "W", "X", "Y", "Z"]], [["E", "S", "O", "V", "P", "Z", "J", "A", "Y", "Q", "U", "I", "R", "H", "X", "L", "N", "F", "T", "G", "K", "D", "C", "M", "W", "B"], ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J*", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W*", "X", "Y", "Z"]], [["V", "Z", "B", "R", "G", "I", "T", "Y", "U", "P", "S", "D", "N", "H", "L", "X", "A", "W", "M", "J", "Q", "O", "F", "E", "C", "K"], ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M*", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z*"]], [["J", "P", "G", "V", "O", "U", "M", "F", "Y", "Q", "B", "E", "N", "H", "Z", "R", "D", "K", "A", "S", "X", "L", "I", "C", "T", "W"], ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L*", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y*", "Z"]], [["N", "Z", "J", "H", "G", "R", "C", "X", "M", "Y", "S", "W", "B", "O", "U", "F", "A", "I", "V", "L", "P", "E", "K", "Q", "D", "T"], ["A", "B", "C", "D", "E", "F", "G", "H*", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U*", "V", "W", "X", "Y", "Z"]], [["F", "K", "Q", "H", "T", "L", "X", "O", "C", "B", "J", "S", "P", "D", "Z", "R", "A", "M", "E", "W", "N", "I", "U", "Y", "G", "V"], ["A", "B", "C*", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P*", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]]], [[["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"], ["L", "U", "S", "N", "P", "Q", "O", "M", "J", "I", "Y", "A", "H", "D", "G", "E", "F", "X", "C", "V", "B", "T", "Z", "R", "K", "W"]], [["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"], ["X", "Q", "U", "M", "F", "E", "P", "O", "W", "L", "T", "J", "D", "Z", "H", "G", "B", "V", "Y", "K", "C", "R", "I", "A", "S", "N"]], [["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"], ["E", "S", "K", "O", "A", "Q", "M", "J", "Y", "H", "C", "P", "G", "T", "D", "L", "F", "U", "B", "N", "R", "X", "Z", "V", "I", "W"]]]]
+EnigmaCycleRotors = [[[["X", "E", "O", "F", "M", "N", "L", "B", "J", "W", "H", "P", "Y", "S", "R", "K", "V", "Z", "U", "Q", "G", "C", "I", "A", "T", "D"], ["A*", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N*", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]], [["Q", "C", "U", "W", "R", "X", "V", "G", "E", "A", "T", "H", "F", "D", "L", "Z", "P", "N", "Y", "J", "K", "O", "B", "S", "I", "M"], ["A", "B", "C", "D", "E", "F*", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S*", "T", "U", "V", "W", "X", "Y", "Z"]], [["P", "W", "G", "X", "F", "E", "N", "S", "Q", "T", "K", "L", "H", "Y", "A", "B", "M", "O", "D", "C", "V", "Z", "U", "J", "R", "I"], ["A", "B*", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O*", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]], [["O", "N", "Q", "F", "S", "X", "T", "D", "A", "I", "L", "K", "B", "C", "J", "Y", "U", "E", "P", "G", "M", "R", "W", "H", "Z", "V"], ["A", "B", "C", "D*", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q*", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]], [["J", "T", "Z", "M", "D", "N", "R", "K", "L", "V", "W", "X", "C", "E", "B", "Q", "P", "A", "O", "I", "F", "Y", "G", "S", "H", "U"], ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K*", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X*", "Y", "Z"]], [["F", "Z", "X", "D", "T", "G", "V", "Y", "W", "J", "K", "N", "S", "C", "M", "A", "L", "B", "E", "Q", "U", "R", "O", "I", "P", "H"], ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M*", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z*"]], [["E", "B", "Q", "I", "G", "L", "D", "H", "V", "P", "F", "U", "Y", "R", "A", "S", "K", "Z", "O", "X", "J", "N", "C", "T", "W", "M"], ["A", "B", "C", "D", "E", "F", "G", "H", "I*", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V*", "W", "X", "Y", "Z"]], [["Y", "S", "L", "B", "C", "X", "Z", "G", "Q", "A", "N", "J", "D", "K", "M", "V", "F", "I", "E", "O", "R", "U", "T", "W", "P", "H"], ["A", "B", "C", "D", "E*", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R*", "S", "T", "U", "V", "W", "X", "Y", "Z"]]], [[["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"], ["S", "O", "N", "L", "R", "T", "V", "Q", "U", "X", "W", "D", "Z", "C", "B", "Y", "H", "E", "A", "F", "I", "G", "K", "J", "P", "M"]], [["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"], ["H", "D", "X", "B", "I", "R", "O", "A", "E", "K", "J", "Q", "P", "Y", "G", "M", "L", "F", "T", "S", "Z", "W", "V", "C", "N", "U"]], [["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"], ["T", "K", "E", "U", "C", "S", "Y", "N", "M", "L", "B", "J", "I", "H", "Q", "X", "O", "Z", "F", "A", "D", "W", "V", "P", "G", "R"]]]]
+
+def main() -> None:
+    print("Command Layout: (FN / EC / C) (FN/EC:#,#... / C:a;1|a;2,a;3|a;4...) (FN/EC:A|B|C / a;1|a;2) (FN: L1L2...Ln) (LR...) (-+...)")
+    infoDump = input("Layout: ").upper().split(" ")
+    
+    rotors = []
+    left = []
+    if infoDump[0] == "FN":
+        rotorLayout = infoDump[1].split(",")[::-1]
+        for i in range(0, len(rotorLayout)):
+            rotors.append(ForgetEnigmaRotors[0][int(rotorLayout[i]) - 1].copy())
+        rotors.append(ForgetEnigmaRotors[1]["ABC".index(infoDump[2])].copy())
+    elif infoDump[0] == "EC":
+        rotorLayout = infoDump[1].split(",")[::-1]
+        for i in range(0, len(rotorLayout)):
+            rotors.append(EnigmaCycleRotors[0][int(rotorLayout[i]) - 1].copy())
+        rotors.append(EnigmaCycleRotors[1]["ABC".index(infoDump[2])].copy())
+    elif infoDump[0] == "C":
+        rotors = infoDump[1].split(",")
+        for i in range(0, len(rotors)):
+            rotors[i] = [rotors[i].split("|")[0].split(";"), rotors[i].split("|")[1].split(";")].copy()
+        rotors.append([infoDump[2].split("|")[0].split(";"), infoDump[2].split("|")[1].split(";")])
+    
+    for i in range(0, len(rotors)):
+        if infoDump[5][i] == "-":
+            rotors[i][0] = rotors[i][0][::-1].copy()
+            rotors[i][1] = rotors[i][1][::-1].copy()
+    
+    setupLetters = list(infoDump[3])[::-1]
+    for i in range(0, len(setupLetters)):
+        while setupLetters[i] not in rotors[i][1][0]:
+            rotors[i] = ShiftSingleRotor(rotors[i], True)
+    
+    leftQueries = []
+    leftQueries = list(infoDump[4])[::-1]
+    for i in range(0, len(rotors) - 1):
+        left.append(leftQueries[i] == "L")
+    
+    processedLetters = []
+    end = False
+    while not end:
+        initialLetters = list(input("Letters to be processed: ").upper())
+        for i in range(0, len(initialLetters)):
+            if initialLetters[i] == "-":
+                end = True
+                continue
+            processedLetters.append(ProcessLetter(initialLetters[i], rotors, left))
+            rotors = ShiftRotors(rotors, left).copy()
+    print("Letters to input: " + "".join(processedLetters))
+
+def ProcessLetter(letter: str, rotors: list, left: list) -> str:
+    intermediate = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ".index(letter), ""]
+    for i in range(0, len(rotors) - 1):
+        intermediate[1] = rotors[i][0][intermediate[0]][:1]
+        intermediate[0] = Find(rotors[i][1], intermediate[1])
+    for i in range(len(rotors) - 1, -1, -1):
+        intermediate[1] = rotors[i][1][intermediate[0]][:1]
+        intermediate[0] = Find(rotors[i][0], intermediate[1])
+    return "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[intermediate[0]]
+
+def Find(l: list, e: str) -> int:
+    for i in range(0, len(l)):
+        if e in l[i]:
+            return i
+    return -1
+
+def ShiftSingleRotor(rotor: list, left: bool) -> list:
+    resultRotor = []
+    for i in range(0, len(rotor)):
+        resultRotor.append(rotor[i][(1 if left else (len(rotor[i]) - 1)):] + rotor[i][:(1 if left else (len(rotor[i]) - 1))])
+    return resultRotor
+
+def ShiftRotors(rotors: list, left: list) -> list:
+    turnOver = []
+    for i in range(0, len(rotors)):
+        turnOver.append("*" in rotors[i][1][0])
+    
+    for i in range(len(rotors) - 2, 0, -1):
+        if sum(turnOver[:i]) == i:
+            rotors[i] = ShiftSingleRotor(rotors[i], left[i]).copy()
+        
+    rotors[0] = ShiftSingleRotor(rotors[0], left[0]).copy()
+    return rotors
+
+if __name__ == "__main__":
+    main()
